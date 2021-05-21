2084dd6cf867a213ccf3cfd21ea239bf19e9a1ad
Rame Madah <lawkira10@gmail.com>
2021-05-21 16:28:38 +0200

    time conversion 100%

--- src/main/java/de/htwberlin/App.java
+++ src/main/java/de/htwberlin/App.java
@@ -7,6 +7,13 @@ public class App {
     String s2 = "07:05:45 AM";
     String s3 = "12:05:45 PM";
     String s4 = "12:05:45 AM";
+    TimeSwitch t = new TimeSwitch();
+
+   // t.timeSwitcher("12:05:45 AM");
+    t.timeSwitcher("07:05:45 PM");
+    t.timeSwitcher("07:05:32 AM");
+   t.timeSwitcher("12:05:29 PM");
+    t.timeSwitcher("12:05:00 AM");
     // TODO: Rufen Sie hier Ihre Methode auf
   }
 }
---
+++ src/main/java/de/htwberlin/TimeSwitch.java
@@ -0,0 +1,59 @@
+package de.htwberlin;
+
+import java.util.ArrayList;
+
+public class TimeSwitch {
+ ArrayList<String> Switcher =  new ArrayList<>();
+
+    public TimeSwitch() {
+    }
+
+    public ArrayList<String> timeSwitcher(String in){
+        String[] time = in.split( "[ :]+");
+        String oldtime;
+        int newtime = 0 ;
+        int converted = 0;
+
+         this.Switcher.clear();
+
+
+        for(String i : time){
+            oldtime = time[0] ;
+
+            if(time[0].equals("12") && time[3].equals("AM") ){
+                converted = 0;
+                time[0]= String.valueOf(converted);
+            }
+            if(time[0].equals("12") && time[3].equals("PM") ){
+                converted = 12;
+                time[0]= String.valueOf(converted);
+            }
+
+            if(i.equals("PM") && converted!= 12){
+                for(int j=0; j<12;j++){
+                    if (newtime < 12)
+                   newtime += 1 ;
+
+                }
+                converted= Integer.parseInt(oldtime) + newtime;
+
+                if(converted >24 ){
+                    converted = 0;
+                }
+                time[0]= String.valueOf(converted);
+
+            }
+
+        }
+        Switcher.add(time[0]);
+        Switcher.add(time[1]);
+        Switcher.add(time[2]);
+        System.out.println("------------");
+
+        System.out.println(Switcher);
+        return this.Switcher;
+    }
+
+
+
+}
